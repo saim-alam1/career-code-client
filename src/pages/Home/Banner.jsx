fc4e672eb7c3a5e1067a1e5b7c927138efdf1b72
saim-alam1 <allbehassansaim@gmail.com>
2025-07-22 16:16:08 +0600

    Banner Image Animation using React Motion

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { motion } from "motion/react";
+import team1 from "../../assets/team/team1.jpg";
+import team2 from "../../assets/team/team2.jpg";
 
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <motion.h1
-            animate={{ rotate: 180, transition: { duration: 4 } }}
+        <div className="flex-1">
+          <motion.img
+            src={team2}
+            animate={{ y: [100, 150, 100] }}
+            transition={{ duration: 4, repeat: Infinity }}
+            className="max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl border-blue-500 border-s-8  border-b-8"
+          />
+          <motion.img
+            src={team1}
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 10, delay: 2, repeat: Infinity }}
+            className="max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl border-blue-500 border-s-8  border-b-8"
+          />
+        </div>
+        <div className="flex-1">
+          {/* <motion.h1
+            animate={{
+              rotate: 180,
+              x: 200,
+              y: -200,
+              transition: { duration: 4 },
+            }}
             className="text-5xl font-bold"
           >
             Latest Jobs For You!
-          </motion.h1>
+          </motion.h1> */}
           <motion.h1
             initial={{ scale: 0 }}
             animate={{ scale: 1, transition: { duration: 4 } }}
