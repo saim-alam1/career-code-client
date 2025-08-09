220ca921648366a0c9f690ff1f68fea749ea3eda
saim-alam1 <allbehassansaim@gmail.com>
2025-08-09 23:44:33 +0600

    Updated Job Application Status

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,14 +1,76 @@
 import { useLoaderData, useParams } from "react-router";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const ViewApplications = () => {
   const { job_id } = useParams();
   const applications = useLoaderData();
   console.log(applications);
+
+  const handleStatusChange = (e, app_id) => {
+    console.log(e.target.value, app_id);
+
+    axios
+      .patch(`http://localhost:3000/applications/${app_id}`, {
+        status: e.target.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.modifiedCount) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Application Status Updated",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1 className="text-3xl text-center my-6">
         {applications.length} Applications for: {job_id}
       </h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {applications.map((application, index) => (
+              <tr key={application._id}>
+                <th>{index + 1}</th>
+                <td>{application.applicant}</td>
+                <td>Quality Control Specialist</td>
+                <td>
+                  <select
+                    onChange={(e) => handleStatusChange(e, application._id)}
+                    defaultValue={application.status}
+                    className="select"
+                  >
+                    <option disabled={true}>Update Status</option>
+                    <option>Pending</option>
+                    <option>Interview</option>
+                    <option>Hired</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
